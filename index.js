4bf27f96e40f1b2d3b2cde7177193cf075644b80
koa0914 <koa0914@naver.com>
2020-08-21 20:52:25 +0900

    bot upload

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = 'process.env.token';
-const welcomeChannelName = "안녕하세요";
-const byeChannelName = "안녕히가세요";
+const token = process.env.token;
+const welcomeChannelName = "👋🏻ㅣ입국";
+const byeChannelName = "👋🏻ㅣ출국";
 const welcomeChannelComment = "어서오세요.";
 const byeChannelComment = "안녕히가세요.";
 
@@ -10,54 +10,72 @@ client.on('ready', () => {
   console.log('켰다.');
 });
 
+client.on("guildMemberAdd", (member) => {
+  const guild = member.guild;
+  const newUser = member.user;
+  const welcomeChannel = guild.channels.find(channel => channel.name == welcomeChannelName);
+
+  welcomeChannel.send(`<@${newUser.id}> ${welcomeChannelComment}\n`);
+
+  member.addRole(guild.roles.find(role => role.name == "닉변 후 회원인증 해주세요"));
+});
+
+client.on("guildMemberRemove", (member) => {
+  const guild = member.guild;
+  const deleteUser = member.user;
+  const byeChannel = guild.channels.find(channel => channel.name == byeChannelName);
+
+  byeChannel.send(`<@${deleteUser.id}> ${byeChannelComment}\n`);
+});
+
 client.on('message', (message) => {
-  if(message.content === '두부는?') {
-    message.reply('귀요미');
+  if(message.author.bot) return;
+
+  if(message.content == 'ping') {
+    return message.reply('pong');
+  }
+    });
+
+    embed.addField('Commands: ', commandStr);
+
+    message.channel.send(embed)
+  }
+
+  if(message.content.startsWith('!전체공지')) {
+    if(checkPermission(message)) return
+    if(message.member != null) { // 채널에서 공지 쓸 때
+      let contents = message.content.slice('!전체공지'.length);
+      message.member.guild.members.array().forEach(x => {
+        if(x.user.bot) return;
+        x.user.send(`<@${message.author.id}> ${contents}`);
+      });
+  
+      return message.reply('공지를 전송했습니다.');
+    } else {
+      return message.reply('채널에서 실행해주세요.');
+    }
   }
 });
 
-client.on("guildMemberAdd", (member) => {
-    const guild = member.guild;
-    const newUser = member.user;
-    const welcomeChannel = guild.channels.find(channel => channel.name == welcomeChannelName);
-  
-    welcomeChannel.send(`<@${newUser.id}> ${welcomeChannelComment}\n`);
-  });
+function checkPermission(message) {
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) {
+    message.channel.send(`<@${message.author.id}> ` + "명령어를 수행할 관리자 권한을 소지하고 있지않습니다.")
+    return true;
+  } else {
+    return false;
+  }
+}
 
-  client.on("guildMemberRemove", (member) => {
-    const guild = member.guild;
-    const deleteUser = member.user;
-    const byeChannel = guild.channels.find(channel => channel.name == byeChannelName);
-  
-    byeChannel.send(`<@${deleteUser.id}> ${byeChannelComment}\n`);
-  });
-  
-  client.on('message', (message) => {
-    if(message.content === 'ping') {
-      message.reply('pong');
-    }
-  });
+function changeCommandStringLength(str, limitLen = 8) {
+  let tmp = str;
+  limitLen -= tmp.length;
 
-  client.on("guildMemberAdd", (member) => {
-    const guild = member.guild;
-    const newUser = member.user;
-    const welcomeChannel = guild.channels.find(channel => channel.name == welcomeChannelName);
-  
-    member.addRole(guild.roles.find(role => role.name == "어서와요"));
-  });
-  
-  client.on("guildMemberRemove", (member) => {
-    const guild = member.guild;
-    const deleteUser = member.user;
-    const byeChannel = guild.channels.find(channel => channel.name == byeChannelName);
-  
-  });
-  
-  client.on('message', (message) => {
-    if(message.author.bot) return;
-  
-    if(message.content === 'ping') {
-      message.reply('pong');
-    }
-  });
+  for(let i=0;i<limitLen;i++) {
+      tmp += ' ';
+  }
+
+  return tmp;
+}
+
+
 client.login(token);
